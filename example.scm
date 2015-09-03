453f361e7e2030aef31ad0905087b173d7fdf54c
amirouche <amirouche.boubekki@gmail.com>
2015-09-04 00:08:25 +0200

    example.scm: better example

--- example.scm
+++ example.scm
@@ -4,26 +4,43 @@
 (define session (pk (session-open connection)))
 
 ;; create a table
-(session-create session "table:nodes" "key_format=Q,value_format=SS")
+(session-create session "table:nodes" "key_format=Q,value_format=SS,columns=(a,b,c)")
+(session-create session "index:nodes:index" "columns=(a, b)")
 
 ;; open a cursor over that table
 (define cursor (pk (cursor-open session "table:nodes")))
 
-;; start a transaction and add a record
+
+;;
+(pk 'set)
 (session-transaction-begin session "isolation=\"snapshot\"")
 
 (cursor-key-set cursor 42)
-(cursor-value-set cursor "The one true number!" "magic happens")
+(cursor-value-set cursor "aaaaaaaaaaaaaa" "bbbbbbbbbbbbbbbbbbb")
+
+(pk 'key (cursor-key-ref cursor))
+(pk 'value (cursor-value-ref cursor))
 
 (cursor-insert cursor)
 
 (session-transaction-commit session)
 (cursor-reset cursor)
 
-;; retrieve record
+;; 
+(pk 'ref)
 (cursor-next cursor)
-(pk (cursor-key-ref cursor))
-(pk (cursor-value-ref cursor))
+(pk 'key (cursor-key-ref cursor))
+(pk 'value (cursor-value-ref cursor))
+
+
+(pk 'index)
+(define index (cursor-open session "index:nodes:index"))
+(cursor-next index)
+(pk 'index-key (cursor-key-ref index))
+(pk 'index-value (cursor-value-ref index))
+
+
+
 (cursor-close cursor)
 (session-close session)
 (connection-close connection)
