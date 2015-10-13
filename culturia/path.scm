97ba6733dbaaaafb874e64eb40886d17bd9afbf1
amirouche <amirouche.boubekki@gmail.com>
2015-10-13 17:21:48 +0200

    add path.scm

---
+++ culturia/path.scm
@@ -0,0 +1,45 @@
+(define-module (path))
+
+
+(define-public (path-join . rest)
+  "Return the absolute path made of REST. If the first item
+   of REST is not absolute the current working directory
+   will be  prepend"
+  (let ((path (string-join rest "/")))
+    (if (string-prefix? "/" path)
+        path
+        (string-append (getcwd) "/" path))))
+
+
+(define-public (path-dfs-walk dirpath proc)
+  (define dir (opendir dirpath))
+  (let loop ()
+    (let ((entry (readdir dir)))
+      (cond
+       ((eof-object? entry))
+       ((or (equal? entry ".") (equal? entry "..")) (loop))
+       (else (let ((path (path-join dirpath entry)))
+               (if (equal? (stat:type (stat path)) 'directory)
+                   (begin (path-dfs-walk path proc)
+                          (proc path))
+                   (begin (proc path) (loop))))))))
+  (closedir dir)
+  (proc (path-join dirpath)))
+
+
+(define-public (rmtree path)
+  (path-dfs-walk path (lambda (path)
+                        (if (equal? (stat:type (stat path)) 'directory)
+                            (rmdir path)
+                            (delete-file path)))))
+
+
+(define-syntax-rule (with-directory path e ...)
+  (begin
+    (when (access? path F_OK)
+      (rmtree path))
+    (mkdir path)
+    e ...
+    (rmtree path)))
+
+(export with-directory)
