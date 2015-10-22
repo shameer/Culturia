4088616bee99f763ee9d94ababd290a11eb83eba
amirouche <amirouche.boubekki@gmail.com>
2015-10-22 14:05:30 +0200

    path: import more procedures

--- culturia/path.scm
+++ culturia/path.scm
@@ -1,5 +1,13 @@
 (define-module (path))
 
+(use-modules (ice-9 optargs))
+
+
+(define (path-exists? path)
+  "Return #true if path is a file or directory.
+   #false if it doesn't exists"
+  (access? path F_OK))
+
 
 (define-public (path-join . rest)
   "Return the absolute path made of REST. If the first item
@@ -9,6 +17,26 @@
     (if (string-prefix? "/" path)
         path
         (string-append (getcwd) "/" path))))
+
+
+(define-public (path-split path)
+  (let ((parts (string-split path #\/)))
+    (if (equal? (car parts) "")
+        (cons (string-append "/" (cadr parts)) (cddr parts))
+        parts)))
+
+
+(define*-public (path-mkdir dirpath #:optional (parents #false))
+  "Create DIRPATH directory and its parents if PARENTS is true"
+  (if parents
+      (let* ((parts (path-split dirpath))
+             (paths (let loop ((dirs (cdr parts))
+                               (out (list (car parts))))
+                      (if (null? dirs)
+                          (reverse out)
+                          (loop (cdr dirs) (cons (apply path-join (list (car out) (car dirs))) out))))))
+        (and (map (lambda (p) (if (not (path-exists? p)) (mkdir p))) paths) #true))
+      (if (not (path-exists? dirpath)) (and (mkdir dirpath) #true))))
 
 
 (define-public (path-dfs-walk dirpath proc)
