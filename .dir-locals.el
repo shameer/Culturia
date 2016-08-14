29e2f4bb661fd9e0b01eadd66364b053f54eafc2
Amirouche <amirouche@hypermove.net>
2016-08-14 15:16:54 +0200

    .dir-locals.el: update

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,8 @@
 ;; The 'nil' configuration applies to all modes.
 ((nil . ((indent-tabs-mode . nil)
-		 (tab-width . 2))))
-
+         (tab-width . 2)
+         (eval . (progn
+                   (put 'for-each-element-in-file 'scheme-indent-function 1)
+                   (put 'test-check 'scheme-indent-function 1)
+                   (put 'call-with-cursor 'scheme-indent-function 2)
+                   (put 'with-cnx 'scheme-indent-function 1))))))
