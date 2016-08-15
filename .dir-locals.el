b6b6b5d145cedac5d554726f6559e38157548827
Amirouche <amirouche@hypermove.net>
2016-08-15 08:52:42 +0200

    .dir-locals.el update

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,6 +2,7 @@
 ((nil . ((indent-tabs-mode . nil)
          (tab-width . 2)
          (eval . (progn
+                   (put 'with-transaction 'scheme-indent-function 1)
                    (put 'for-each-element-in-file 'scheme-indent-function 1)
                    (put 'test-check 'scheme-indent-function 1)
                    (put 'call-with-cursor 'scheme-indent-function 2)
