399c9bf4a248460815d1dc1e388d34a9cebe17a7
Amirouche <amirouche@hypermove.net>
2016-08-17 21:45:21 +0200

    add urls-step00-fetch.scm

---
+++ src/urls-step00-fetch.scm
@@ -0,0 +1,29 @@
+(use-modules (srfi srfi-1))
+(use-modules (rnrs io ports))
+(use-modules (ice-9 receive))
+(use-modules (ice-9 threads))
+(use-modules (http))
+(use-modules (json))
+(use-modules (msgpack))
+(use-modules (ice-9 rdelim))
+
+
+;;;
+;;; Input must be one url per line.  For each url, output a pair made
+;;; of the url and `curl -is ,url' output
+;;;
+
+
+(define (maybe-curl url)
+  (catch #t
+    (lambda () (cons url (curl url)))
+    (lambda _ '())))
+
+
+(define urls (let loop ((line (read-line))
+                        (out '()))
+               (if (eof-object? line)
+                   out
+                   (loop (read-line) (cons line out)))))
+
+(n-for-each-par-map 8 write maybe-curl urls)
