f73a6b4eab9dbbed39f9da03b6daf8f6ac9aa4be
Amirouche Boubekki <amirouche@hypermove.net>
2016-08-18 12:49:59 +0200

    hn-step00-dump-hn-api: small improvements
    - output scm instead of msgpack
    - use 20 threads instead of 8
    - log current uid

--- src/hn-step00-dump-hn-api.scm
+++ src/hn-step00-dump-hn-api.scm
@@ -16,20 +16,24 @@
 (define (download uid)
   (catch #t
     (lambda ()
-      (let* ((url "https://hacker-news.firebaseio.com/v0/item/~a.json")
-             (url (format #f url (1+ uid))))
-        (pack (curl url))))
-    (lambda _ '())))
+      (let* ((uid (1+ uid))
+             (url "https://hacker-news.firebaseio.com/v0/item/~a.json")
+             (url (format #f url uid)))
+        (cons uid
+              (call-with-output-string
+               (lambda (port)
+                 (write (curl url) port))))))
+    (lambda _ "")))
 
-(define (store bv)
-  (if (null? bv)
-    (display "0")
-    (let ((port (open-file "hn.msgpack" "ab")))
-      (display "1")
-      (put-bytevector port bv)
+(define (store pair)
+  (if (equal? string "")
+    (pk "0")
+    (let ((port (open-file "hn.scm" "a")))
+      (pk (car pair))
+      (put-string port (cdr pair))
       (close port))))
 
 (define (dump)
-  (n-for-each-par-map 8 store download (iota (max-id))))
+  (n-for-each-par-map 20 store download (iota 100)))
 
 (dump)
