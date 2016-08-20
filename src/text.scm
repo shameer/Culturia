220b789512e5e0c58ff8a887c822ed78ee477432
Amirouche <amirouche@hypermove.net>
2016-08-20 10:45:23 +0200

    add text.scm

---
+++ src/text.scm
@@ -0,0 +1,66 @@
+(define-module (text))
+
+(use-modules (ice-9 popen))
+(use-modules (ice-9 rdelim))
+(use-modules (srfi srfi-1))
+(use-modules (srfi srfi-26))
+
+
+;;;
+;;; wrapping html2text
+;;;
+;;
+;; inspired from ice-9 popen
+;;
+(use-modules (ice-9 popen))
+
+(define open-process (@@ (ice-9 popen) open-process))
+
+(define (html2text string)
+  (call-with-values (lambda () (open-process OPEN_BOTH "html2text"))
+    (lambda (read-port write-port pid)
+      (display string write-port)
+      (close-port write-port)
+      (let ((str (read-string read-port)))
+        (close-port read-port)
+        (waitpid pid)
+        str))))
+
+;;;
+;;; tokenizing
+;;;
+
+;; english stop words
+;; XXX: actually not very useful
+
+(define stopwords (make-hash-table))
+
+(with-input-from-file "stopwords.en.txt"  ;; snarffed from http://www.ranks.nl/stopwords/
+  (lambda ()
+    (let loop ((stopword (read-line)))
+      (unless (eof-object? stopword)
+        (hash-set! stopwords stopword #t)
+        (loop (read-line))))))
+
+(define (filter-stopwords lst)
+  (filter (lambda (token) (not (hash-ref stopwords token))) lst))
+
+;; tokens
+
+(define punctuation (string->list "!\"#$%&\\'()*+,-./:;<=>?@[\\]^_`{|}~"))
+
+(define (clean text)
+  "Replace punctuation characters from TEXT with a space character"
+  (string-map (lambda (char) (if (list-index (cut equal? char <>) punctuation) #\space char)) text))
+
+(define (clean-newlines text)
+  (string-map (lambda (char) (if (eq? #\newline char) #\space char)) text))
+
+(define split (cut string-split <> #\space))
+
+(define (sanitize words)
+  "Only keep words that have length bigger than one"
+  (filter (lambda (word) (< 1 (string-length word))) words))
+
+;; XXX: compose must be read from right to left
+(define-public string->tokens (compose filter-stopwords sanitize split string-downcase clean-newlines clean))
