826c93de921e14670d6ee0ef4c045ecf4109d29c
Amirouche <amirouche@hypermove.net>
2016-08-21 11:15:44 +0200

    cosmit

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,6 +2,7 @@
 ((nil . ((indent-tabs-mode . nil)
          (tab-width . 2)
          (eval . (progn
+                   (put 'with-cursor 'scheme-indent-function 1)
                    (put 'with-transaction 'scheme-indent-function 1)
                    (put 'stream-let 'scheme-indent-function 2)
                    (put 'for-each-element-in-file 'scheme-indent-function 1)
