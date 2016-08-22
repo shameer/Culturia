6455e401ba747854cf5644b884491fffa6fb1c98
Amirouche Boubekki <amirouche@hypermove.net>
2016-08-22 21:37:52 +0200

    cosmit

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,6 +3,7 @@
          (tab-width . 2)
          (eval . (progn
                    (put 'with-cursor 'scheme-indent-function 1)
+                   (put 'match 'scheme-indent-function 1)
                    (put 'with-transaction 'scheme-indent-function 1)
                    (put 'stream-let 'scheme-indent-function 2)
                    (put 'for-each-element-in-file 'scheme-indent-function 1)
