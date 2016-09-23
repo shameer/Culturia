d1323cbe008e8689fb1875486fbf4e650c29aacc
Amirouche <amirouche@hypermove.net>
2016-09-24 00:01:59 +0200

    wsh: replace the old shenanigan about document and url
    The signature of `index` changed to `(index document html)` where
    html can be any string identifier.

--- src/wsh.scm
+++ src/wsh.scm
@@ -11,39 +11,18 @@
 (use-modules (text))
 
 
-(define-public *wsh* '((documents
-                        ((url . string))
-                        ((title . string)
-                         (snippet . string))
-                        ())
-                       (urls
-                        ((uid . record))
-                        ((url . string))
-                        ())
-                       (terms
+(define-public *wsh* '((terms
                         ((uid . record))
                         ((string . string))
                         ((inverse (string) (uid))))
                        (inverted-index
                         ((term . unsigned-integer)
-                         (url-uid . unsigned-integer)
+                         (document . string)
                          (position . unsigned-integer))
                         ((nothing . bytes))
-                        ((positions (url-uid position) (term))))))
+                        ((positions (document position) (term))))))
 
-(define-public (add-document url title snippet)
-  (call-with-cursor 'documents
-    (lambda (cursor)
-      (cursor-insert* cursor (list url) (list title snippet)))))
-
-(define-public (document-ref url)
-  (call-with-cursor 'documents
-    (lambda (cursor)
-      (catch 'wiredtiger
-        (lambda () (cursor-value-ref* cursor url))
-        (lambda (key . args) #f)))))
-
-(define (index-term-with-position url-uid)
+(define (index-term-with-position document)
   (match-lambda
     ((term position)
      (let ((uid ;; get or create term
@@ -56,17 +35,13 @@
                                 (cursor-insert* cursor '() (list term))))))))))
        (call-with-cursor 'inverted-index
          (lambda (cursor)
-           (cursor-insert* cursor (list uid url-uid position) (list #vu8()))))))))
+           (cursor-insert* cursor (list uid document position) (list #vu8()))))))))
 
-(define-public (index url html)
-  (call-with-cursor 'urls-append
-    (lambda (cursor)
-      ;; insert url and retrieve its uid
-      (let ((url-uid (cursor-insert* cursor '() (list url))))
-        ;; convert html to tokens and add positions
-        (let* ((terms (html->tokens html))
-               (terms+positions (zip terms (iota (length terms)))))
-          (for-each (index-term-with-position url-uid) terms+positions))))))
+(define-public (index document html)
+  ;; convert html to tokens and add positions
+  (let* ((terms (html->tokens html))
+         (terms+positions (zip terms (iota (length terms)))))
+    (for-each (index-term-with-position document) terms+positions)))
 
 (define (uid->url uid)
   (call-with-cursor 'urls
@@ -87,7 +62,7 @@
         (call-with-cursor 'inverted-index
           (lambda (cursor)
             ;; return document-id and position
-            (delete-duplicates (map cadar (cursor-range-prefix cursor uid 0 0))))))))
+            (delete-duplicates (map cadar (cursor-range-prefix cursor uid "" 0))))))))
 
 (define-public (debug)
   (call-with-cursor 'inverted-index
@@ -115,22 +90,6 @@
   (let ((tokens (filter (lambda (x) (not (equal? x ""))) (string-split string #\space))))
     (apply query/and (map query% tokens))))
         
-(define true? (cut eq? #t <>))
-
-(define (search/make-predicate arg)
-  (match arg
-    (('term . term) (let ((termid (term-uid term)))
-                      (lambda (docid)
-                        (call-with-cursor 'inverted-index
-                          (lambda (cursor)
-                            (not (zero? (cursor-count-prefix cursor termid docid 0))))))))
-    (('and . args) (let ((predicates (map (cut search/make-predicate <>) args)))
-                     (lambda (docid)
-                       (every true? (map (cut <> docid) predicates)))))
-    (('or . args) (let ((predicates (map (cut search/make-predicate <>) args)))
-                    (lambda (docid)
-                      (any true? (map (cut <> docid) predicates)))))))
-
 (define (search/vm query)
   (match query
     (('term . term) (search/term term))
@@ -140,7 +99,7 @@
             (others (lset-difference equal? args nots)))
        ;; retrieve hits before applying 'not'
        (let* ((nots (map search/vm nots))
-              (hits (apply lset-intersection (cons eq? (map search/vm others)))))
+              (hits (apply lset-intersection (cons equal? (map search/vm others)))))
          (let loop ((nots nots)
                     (hits hits))
            (cond
@@ -149,7 +108,7 @@
             (else (loop (cdr nots) ((car nots) hits))))))))
     (('or . args) (delete-duplicates (append-map search/vm args)))
     (('not . arg) (lambda (hits)
-                    (lset-difference eq? hits (search/vm arg))))))
+                    (lset-difference equal? hits (search/vm arg))))))
 
 (define (flatten lst)
   (let loop ((lst lst)
@@ -173,15 +132,15 @@
   "convert QUERY to a list of relevant terms for computing the score"
   (flatten (query-terms% query)))
 
-(define (term-frequency term-id doc-id)
-  "frequency of TERM-ID in DOC-ID"
+(define (term-frequency term-id document)
+  "frequency of TERM-ID in DOCUMENT"
   (call-with-cursor 'inverted-index
     (lambda (cursor)
-      (cursor-count-prefix cursor term-id doc-id 0))))
+      (cursor-count-prefix cursor term-id document 0))))
 
-(define (score term-ids doc-id)
-  "score DOC-ID against TERM-IDS"
-  (apply + (map (cut term-frequency <> doc-id) term-ids)))
+(define (score term-ids document)
+  "score DOCUMENT against TERM-IDS"
+  (apply + (map (cut term-frequency <> document) term-ids)))
 
 (define-public (search* query)
   "retrieve sorted urls for QUERY"
@@ -191,8 +150,7 @@
     (let ((term-ids (query-terms query)))
       ;; score every hits against terms
       (let ((scores (map (cut score term-ids <>) hits)))
-        (let ((urls (map uid->url hits)))
-          (sort (map cons urls scores) (lambda (a b) (> (cdr a) (cdr b))) ))))))
+        (sort (map cons hits scores) (lambda (a b) (> (cdr a) (cdr b))) )))))
 
 ;;;
 ;;; tests
@@ -243,107 +201,65 @@
 
   (test-check "search/vm and/or"
     (with-env (env-open* "/tmp/wt" *wsh*)
-      (index "http://example.net" "database & postgresql")
-      (index "http://example.net" "spam & pgsql")
-      (index "http://example.net" "spam & egg")
-      (index "http://example.net" "database & egg")
-      (index "http://example.net" "database & pgsql")
+      (index "1" "database & postgresql")
+      (index "2" "spam & pgsql")
+      (index "3" "spam & egg")
+      (index "4" "database & egg")
+      (index "5" "database & pgsql")
       (search/vm (query/and (query/term "database") (query/or (query/term "postgresql")
-                                                                 (query/term "pgsql")))))
-    '(5 1))
+                                                              (query/term "pgsql")))))
+    '("5" "1"))
 
   (test-check "search/vm or avoid duplicates"
     (with-env (env-open* "/tmp/wt" *wsh*)
-      (index "http://example.net" "database")
-      (index "http://example.net" "wiredtiger & database")
-      (index "http://example.net" "wiredtiger")
+      (index "1" "database")
+      (index "2" "wiredtiger & database")
+      (index "3" "wiredtiger")
       
       (search/vm (query/or (query/term "database")
                             (query/term "wiredtiger"))))
-    '(2 1 3))
+    '("2" "1" "3"))
 
   (test-check "search/vm avoid duplicates"
     (with-env (env-open* "/tmp/wt" *wsh*)
-      (index "http://example.net" "wiredtiger & wiredtiger")
+      (index "1" "wiredtiger & wiredtiger")
       (search/vm (query/term "wiredtiger")))
-    '(1))
+    '("1"))
 
   (test-check "search/vm and/not"
     (with-env (env-open* "/tmp/wt" *wsh*)
-      (index "http://example.net" "database & postgresql")
-      (index "http://example.net" "spam & pgsql")
-      (index "http://example.net" "spam & egg")
-      (index "http://example.net" "database & egg")
-      (index "http://example.net" "database & pgsql")
+      (index "1" "database & postgresql")
+      (index "2" "spam & pgsql")
+      (index "3" "spam & egg")
+      (index "4" "database & egg")
+      (index "5" "database & pgsql")
       (search/vm (query/and (query/term "database")
                              (query/not (query/term "egg")))))
-  '(5 1))
+  '("5" "1"))
 
   (test-check "search/vm and/not/and"
     (with-env (env-open* "/tmp/wt" *wsh*)
-      (index "http://example.net" "database & egg")
-      (index "http://example.net" "database & spam & egg")
-      (index "http://example.net" "database & spam & egg")
-      (index "http://example.net" "database & egg")
-      (index "http://example.net" "database & spam")
+      (index "1" "database & egg")
+      (index "2" "database & spam & egg")
+      (index "3" "database & spam & egg")
+      (index "4" "database & egg")
+      (index "5" "database & spam")
       (search/vm (query/and (query/term "database") (query/not (query/and (query/term "egg")
                                                                               (query/term "spam"))))))
-    '(5 4 1))
+    '("5" "4" "1"))
 
   (test-check "search/vm and/not"
     (with-env (env-open* "/tmp/wt" *wsh*)
-      (index "http://example.net" "database & postgresql")
-      (index "http://example.net" "spam & pgsql")
-      (index "http://example.net" "spam & egg")
-      (index "http://example.net" "database & egg")
-      (index "http://example.net" "database & pgsql")
+      (index "1" "database & postgresql")
+      (index "2" "spam & pgsql")
+      (index "3" "spam & egg")
+      (index "4" "database & egg")
+      (index "5" "database & pgsql")
       (search/vm (query/and (query/term "database")
                              (query/not (query/or (query/term "egg")
                                                     (query/term "pgsql"))))))
-    '(1))
+    '("1"))
   
-  (test-check "search/make-predicate 1"
-    (with-env (env-open* "/tmp/wt" *wsh*)
-      (index "http://example.net" "database & postgresql")
-      (index "http://example.net" "spam & pgsql")
-      (index "http://example.net" "spam & egg")
-      (index "http://example.net" "database & egg")
-      (index "http://example.net" "database & pgsql & spam")
-      (let* ((query (query/term "database"))
-             (predicate (search/make-predicate query)))
-        
-        (filter predicate (map (cut + 1 <>) (iota 5)))))
-  '(1 4 5))
-
-  (test-check "search/make-predicate 2"
-    (with-env (env-open* "/tmp/wt" *wsh*)
-      (index "http://example.net" "database & postgresql")
-      (index "http://example.net" "spam & pgsql")
-      (index "http://example.net" "spam & egg")
-      (index "http://example.net" "database & egg")
-      (index "http://example.net" "database & pgsql & spam")
-      (let* ((query (query/and (query/term "database")
-                                (query/term "postgresql")))
-             (predicate (search/make-predicate query)))
-        
-        (filter predicate (map (cut + 1 <>) (iota 5)))))
-  '(1))
-
-  (test-check "search/make-predicate 3"
-    (with-env (env-open* "/tmp/wt" *wsh*)
-      (index "http://example.net" "database & postgresql")
-      (index "http://example.net" "spam & pgsql")
-      (index "http://example.net" "spam & egg")
-      (index "http://example.net" "database & egg")
-      (index "http://example.net" "database & pgsql & spam")
-      (let* ((query (query/and (query/term "database")
-                                (query/or (query/term "postgresql")
-                                           (query/term "pgsql"))))
-             (predicate (search/make-predicate query)))
-        
-        (filter predicate (map (cut + 1 <>) (iota 5)))))
-  '(1 5))
-
   (test-check "search*"
     (with-env (env-open* "/tmp/wt" *wsh*)
       (index "http://database.postgresql.pgsql.net" "database & postgresql & pgsql")
@@ -356,15 +272,18 @@
                                (query/or (query/term "postgresql")
                                           (query/term "pgsql")))))
         (search* query)))
-    '(("http://database.postgresql.pgsql.net/database" . 4) ("http://database.postgresql.pgsql.net" . 3) ("http://database.pgsql.spam.net" . 2) ("http://database.postgresql.net" . 2)))
+    '(("http://database.postgresql.pgsql.net/database" . 4)
+      ("http://database.postgresql.pgsql.net" . 3)
+      ("http://database.postgresql.net" . 2)
+      ("http://database.pgsql.spam.net" . 2)))
 
   (test-check "search/vm unknown keyword"
     (with-env (env-open* "/tmp/wt" *wsh*)
-      (index "http://example.net" "database & postgresql")
-      (index "http://example.net" "spam & pgsql")
-      (index "http://example.net" "spam & egg")
-      (index "http://example.net" "database & egg")
-      (index "http://example.net" "database & pgsql")
+      (index "1" "database & postgresql")
+      (index "2" "spam & pgsql")
+      (index "3" "spam & egg")
+      (index "4" "database & egg")
+      (index "5" "database & pgsql")
       (search/vm (query/and (query/term "wiredtiger"))))
     '())
 
